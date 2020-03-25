1708f595c66a4469706e17731ced43d167bfb8d5
linxuanyang <yanglinxuan525@gmail.com>
2020-03-25 18:30:54 -0400

    add jsonl for doccano

--- write_jsonl.py
+++ write_jsonl.py
@@ -1,14 +1,14 @@
 import json
 import jsonlines
 def write_jsonline(json_file):
-    with jsonlines.open("./data/sanders.jsonl",mode='w') as writer:
+    with jsonlines.open("./data/yang.jsonl",mode='w') as writer:
         with open(json_file) as f:
             data=json.load(f)
             for video in data:
                 for author in data[video]:
                     comment=data[video][author]
-                    dict={"text":video + "\n\n" + author + "\n\n" + comment}
+                    dict={"text":video + " author: " + author + " " + comment}
                     writer.write(dict)
 
 
-write_jsonline("./data/sanders_preprocessed.json")
+write_jsonline("./data/yang_preprocessed.json")
