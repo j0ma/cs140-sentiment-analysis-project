3c955e63bd64c58609d66e6d3efecdf5a34db644
Jonne Saleva <jonne@jonnesaleva.com>
2020-03-29 18:10:26 -0400

    functionality for searching for a comment

--- helpers.py
+++ helpers.py
@@ -1,8 +1,6 @@
 import json
 import pandas as pd
 
-with open('./data/all_comments.json', 'r') as f:
-    comments_dataset = json.loads(f.read())
 
 def triplets(arr):
     """
@@ -21,3 +19,26 @@ def load_comments(candidate_name=None):
     else:
         return pd.read_csv(f'data/{candidate_name}.csv', lineterminator='\n')
 
+with open('./data/all_comments.json', 'r') as f:
+    comments_dataset = json.loads(f.read())
+
+all_comments_df = load_comments('all')
+
+def make_rows_readable(df):
+    out = []
+    for ix, row in df.iterrows():
+        pretty = []
+        for c in row.index:
+            pretty.append(f"{c}: {row[c]}")
+        pretty = "\n".join(pretty)
+        out.append(pretty)
+    return "\n#####\n".join(out)
+
+def search_for_comment(s, k=1):
+    """
+    Finds the top k results containing s in the comment string.
+    """
+
+    subset = all_comments_df[all_comments_df.comment.str.contains(s)].head(k)
+    output = make_rows_readable(subset)
+    return output
